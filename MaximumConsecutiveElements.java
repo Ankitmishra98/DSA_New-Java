825d97e196881adb31ad8167741953c2c9f5b1e3
Ankitmishra98 <166702444+Ankitmishra98@users.noreply.github.com>
2026-08-20 20:03:18 +0530

    Update MaximumConsecutiveElements.java

--- MaximumConsecutiveElements.java
+++ MaximumConsecutiveElements.java
@@ -1,22 +1,20 @@
-
-
-public class Main
-{
-	public static void main(String[] args) {
-	int[] arr={1,1,0,0,1,0,1,1,1,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,};
-	int count=0; int max=0; int ele=arr[0];
-	for(int i=0; i<arr.length-1; i++){
-	    if(arr[i]==arr[i+1]){
-	        count++;
-	        if(max <count){
-	            max=count;
-	            ele=arr[i];
-	        }
-	    }
-	    else{
-	        count=0;
-	    }
-	}
-	System.out.println("Element: "+ele+" max: "+max);
-	}
+class Solution {
+    public int maxConsecBits(int[] arr) {
+        // code here
+        int max=1; int count=1;
+        for(int i=0; i<arr.length-1;i++){
+            if(arr[i] == arr[i+1]){
+                count++;
+                if(max<count){
+                    max=count;
+                }
+            }
+            else{
+                count=1;
+            }
+            }
+        
+        return max;
+    }
 }
+
