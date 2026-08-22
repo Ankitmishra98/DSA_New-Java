16feb48a7cbfbc7ec0aca09ec9253c08cb4437c8
Ankitmishra98 <166702444+Ankitmishra98@users.noreply.github.com>
2026-08-22 13:45:50 +0530

    Create LongestSubarrayWithGivenSumBruteForce.java

---
+++ LongestSubarrayWithGivenSumBruteForce.java
@@ -0,0 +1,24 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+int[] arr={0,1,2,3,1,1,1,1,2,4}; int max=0; int tar=4;
+for(int i=0; i<arr.length; i++){
+    for(int j=i; j<arr.length; j++){
+        int sum=0;
+        for(int k=i; k<=j; k++){
+            // System.out.print(arr[k]+"   ");
+            sum+=arr[k];
+        }
+       if(sum==tar){
+            if(max < (j-i+1)){
+            max=j-i+1;
+        }
+       }
+        // System.out.println(" J-I: "+(j-i+1));
+    }
+}
+System.out.println(max);
+	}
+}
